16ec1d149703b7e16996ff1025f6ee81578622fb
Alisher <kulm.alisher@gmail.com>
2024-10-11 14:48:41 -0500

    added headers extraction for proxy

--- GptVPN.py
+++ GptVPN.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import requests
 
 app = Flask(__name__)
@@ -10,24 +10,38 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 @app.route("/<path:endpoint>", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
 def proxy_request(endpoint):
-
     # check proxy auth
     if request.headers.get('X-PROXY-AUTH-KEY') != PROXY_AUTH_KEY:
         return jsonify('Unauthorized'), 401
 
     # prepare data
     url = f"{OPENAI_URL}/{endpoint}"
-    headers = {'Authorization': request.headers.get('Authorization')}
+    headers = {
+        'Authorization': request.headers.get('Authorization'),
+        }
 
     # get response from openai
     response = requests.request(
         url=url,
         method=request.method,
         json=request.json,
-        headers=headers
+        headers=headers,
     )
 
-    return response.json(), response.status_code
+
+    # Extract important headers
+    response_headers = []
+    meta_headers = [
+        'openai-organization', 'openai-processing-ms', 'openai-version', 'x-request-id',
+        'x-ratelimit-limit-requests', 'x-ratelimit-limit-tokens', 'x-ratelimit-remaining-requests',
+        'x-ratelimit-remaining-tokens', 'x-ratelimit-reset-requests', 'x-ratelimit-reset-tokens'
+    ]
+
+    for header in meta_headers:
+        if response.headers.get(header):
+            response_headers.append((header, response.headers.get(header)))
+    return Response(response.content, response.status_code, response_headers)
+
 
 if __name__ == "__main__":
     app.run()
